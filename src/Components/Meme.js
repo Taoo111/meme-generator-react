48e5f73b8e72f84621807efd7fab297e97cc8226
Dawid <dawidjura12@gmail.com>
2022-07-15 11:11:55 +0200

    Fixed bug with default img on meme img container

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,34 +1,37 @@
-
-import memesData from '../memesData.js'
-import React from 'react'
-import smileFace from '../Images/smile.jpg'
-
+import memesData from "../memesData.js";
+import React from "react";
+import smileFace from "../Images/smile.jpg";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState(smileFace);
 
-    const memeContainer = document.querySelector('.meme--img')
-    
-    // eslint-disable-next-line no-undef
-    const [meme, setMeme] = React.useState("");
-    
-    const getImg = () => {
-        const randomIndex = Math.floor(Math.random() * memesData.data.memes.length);
-        const url = memesData.data.memes[randomIndex].url;
-        setMeme(url);
+  const getImg = () => {
+    const randomIndex = Math.floor(Math.random() * memesData.data.memes.length);
+    const url = memesData.data.memes[randomIndex].url;
+    setMeme(url);
+  };
+  console.log(meme);
 
-        memeContainer.attributes.src.value = meme;
-    }
-
-    return(
-        <main>
-            <div className="form">
-                <input className="form--input" type='text' placeholder="Top text"></input>
-                <input className="form--input" type='text' placeholder="Bottom text"></input>
-                <button className="form--button" onClick={getImg}>Get a new meme image</button>
-            </div>
-            <div className="meme">
-                <img className="meme--img" src={smileFace} alt="meme"></img>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="form">
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Top text"
+        ></input>
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Bottom text"
+        ></input>
+        <button className="form--button" onClick={getImg}>
+          Get a new meme image
+        </button>
+      </div>
+      <div className="meme">
+        <img className="meme--img" src={meme} alt="meme"></img>
+      </div>
+    </main>
+  );
 }
